80702502663e15477306543e65a7f314f2d7c876
Musa Zeshan <musazeshan.it@gmail.com>
2024-06-13 00:04:02 +0500

    The navbar has been made.

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-
-        </div>
+        <nav className='flex justify-center gap-14 border-y-2 border-black py-5 tracking-wider'>
+            <Link>HOME</Link>
+            <Link>CLOTHES</Link>
+            <Link>ACCESSORIES</Link>
+            <Link>APPLIANCES</Link>
+            <Link>LOJA PRODUCTS</Link>
+            <Link>ABOUT US</Link>
+        </nav>
     );
 }
 
