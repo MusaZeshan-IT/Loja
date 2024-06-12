d28993911b87085377a477ceb3464f98bc097991
Musa Zeshan <musazeshan.it@gmail.com>
2024-06-12 23:58:04 +0500

    Now the main header which says the contact page and the login page link with the brand logo has been completed/built.

--- src/components/common/HeaderMain.jsx
+++ src/components/common/HeaderMain.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/loja-logo.png'
 
 const HeaderMain = () => {
     return (
-        <header></header>
+        <header className='flex justify-between items-center py-10 px-52'>
+            <span className='text-3xl'>
+                <Link>CONTACT US</Link>
+            </span>
+            <img className='h-32' src={Logo} alt="" />
+            <span className='text-3xl'>
+                <Link>LOGIN / REGISTER</Link>
+            </span>
+        </header>
     );
 }
 
