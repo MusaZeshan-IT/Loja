399b3123f3f9a384af1773c6bf40b22a8eafc187
Musa Zeshan <musazeshan.it@gmail.com>
2024-06-12 23:52:44 +0500

    The header is built.

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <header className='flex justify-between items-center bg-[#bc8e74] py-4 px-16'>
+            <div className='text-white tracking-widest text-2xl'>
+                <span className='me-20'>PAKISTAN</span>
+                <span>FREE DELIVERY</span>
+            </div>
+            <div className='text-white'>
+                <i className='fa-brands fa-instagram text-5xl me-10'></i>
+                <i className='fa-brands fa-facebook text-5xl'></i>
+            </div>
+        </header>
     );
 }
 
