dab1ee45da4dfb57c43401bbb30b9f892f379319
Musa Zeshan <musazeshan.it@gmail.com>
2024-06-24 20:51:56 +0500

    The images on the appliances page are added.

--- src/helpers/AppliancesList.js
+++ src/helpers/AppliancesList.js
@@ -1,10 +1,10 @@
-// // APPLIANCES IMAGES
-// import Appliances1 from '../assets/appliances/img1.jpg'
-// import Appliances2 from '../assets/appliances/img2.jpg'
-// import Appliances3 from '../assets/appliances/img3.jpg'
-// import Appliances4 from '../assets/appliances/img4.jpg'
-// import Appliances5 from '../assets/appliances/img5.jpg'
-// import Appliances6 from '../assets/appliances/img6.jpg'
+// APPLIANCES IMAGES
+import Appliances1 from '../assets/appliances/fridge.jpg'
+import Appliances2 from '../assets/appliances/milk-shaker2.jpg'
+import Appliances3 from '../assets/appliances/modern-tap.jpg'
+import Appliances4 from '../assets/appliances/portable-fan.jpg'
+import Appliances5 from '../assets/appliances/milk-shaker3.jpg'
+import Appliances6 from '../assets/appliances/juice-blender.jpg'
 
 const AppliancesList = () => {
     return (
@@ -13,37 +13,37 @@ const AppliancesList = () => {
                 id: 1,
                 name: "T-SHIRT",
                 price: "29.99",
-                // url: Appliances1,
+                url: Appliances1,
             },
             {
                 id: 2,
                 name: "BLACK SHIRT",
                 price: "78",
-                // url: Appliances2,
+                url: Appliances2,
             },
             {
                 id: 3,
                 name: "COAT",
                 price: "67",
-                // url: Appliances3,
+                url: Appliances3,
             },
             {
                 id: 4,
                 name: "GRAY PANT",
                 price: "19",
-                // url: Appliances4,
+                url: Appliances4,
             },
             {
                 id: 5,
                 name: "DRESS PANT",
                 price: "54.89",
-                // url: Appliances5,
+                url: Appliances5,
             },
             {
                 id: 6,
                 name: "BLUE JEANS",
                 price: "31.76",
-                // url: Appliances6,
+                url: Appliances6,
             },
         ]
     )
--- src/pages/Appliances.jsx
+++ src/pages/Appliances.jsx
@@ -5,8 +5,12 @@ import CTA from '../components/shared/CTA';
 import AppliancesList from '../helpers/AppliancesList';
 import CategoryCard from '../components/shared/CategoryCard';
 
-const Appliances = () => {
+//Images
+import AppliancesCatImg1 from '../assets/appliances/fridge.jpg';
+import AppliancesCatImg2 from '../assets/appliances/modern-tap.jpg';
+import AppliancesCatImg3 from '../assets/appliances/steamer.jpg';
 
+const Appliances = () => {
 
     return (
         <div>
@@ -18,7 +22,7 @@ const Appliances = () => {
                     <div className='grid lg-custom:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-16 gap-y-24'>
                         {AppliancesList().map((p) => {
                             return (
-                                <ProductCard key={p.id} name={p.name} price={p.price} />
+                                <ProductCard key={p.id} name={p.name} bgimgUrl={p.url} price={p.price} />
                             )
                         })}
                     </div>
@@ -30,9 +34,9 @@ const Appliances = () => {
             <CoverImage isCTA={false} />
             <div className='2xl:px-28 xl-custom:px-24 xl:px-20 lg-custom:px-16 lg:px-14 px-12'>
                 <div className='grid md:grid-cols-3 sm-custom:grid-cols-2 grid-cols-1 mt-28 gap-16'>
-                    <CategoryCard name="HOME APPLIANCES" />
-                    <CategoryCard name="OFFICE APPLIANCES" />
-                    <CategoryCard name="SMART APPLIANCES" />
+                    <CategoryCard name="HOME APPLIANCES" bgImgUrl={AppliancesCatImg1} />
+                    <CategoryCard name="OFFICE APPLIANCES" bgImgUrl={AppliancesCatImg2} />
+                    <CategoryCard name="SMART APPLIANCES" bgImgUrl={AppliancesCatImg3} />
                 </div>
             </div>
         </div>
